e1942c96aaf0766b1d7343023479772bc4d0ac91
viktor trokhymenko <trokhymenkoviktor@gmail.com>
2020-05-08 23:56:11 +0300

    update name lice

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,13 +1,31 @@
 import re
 import sys
+from typing import Callable, List
+
+MODULE_REGEX = r"^[a-z][a-z0-9\-]+[a-z0-9]$"
+module_name = "{{ cookiecutter.project_name }}"
 
 
-MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+def validate_project_name() -> None:
+    """This validator is used to ensure that `project_name` is valid.
 
-project_name = "{{ cookiecutter.project_name }}"
+    Valid inputs starts with the lowercase letter.
+    Followed by any lowercase letters, numbers or underscores.
 
-if not re.match(MODULE_REGEX, project_name):
-    print(f"error: {project_name} is not a valid module name!")
+    Raises:
+        ValueError: If module_name is not a valid Python module name
+    """
+    if not re.match(MODULE_REGEX, module_name):
+        message = f"ERROR: The project name `{module_name}` is not a valid Python module name."
 
-    # exits with status 1 to indicate failure
-    sys.exit(1)
+        raise ValueError(message)
+
+
+validators: List[Callable[[], None]] = [validate_project_name]
+
+for validator in validators:
+    try:
+        validator()
+    except ValueError as ex:
+        print(ex)
+        sys.exit(1)
