6b04ca08404870b63f19656ac9a293147a725e84
viktor trokhymenko <trokhymenkoviktor@gmail.com>
2020-05-08 22:05:17 +0300

    update hooks/*.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,18 +1,18 @@
-"""This module is called after project is created."""
+"""this module is called after project is created"""
 
 import os
 import textwrap
 from pathlib import Path
 
-# Get the root project directory:
+# get the root project directory:
 PROJECT_DIRECTORY = Path.cwd().absolute()
 PROJECT_NAME = "{{ cookiecutter.project_name }}"
 
-# We need these values to generate correct license:
+# generate correct license:
 LICENSE = "{{ cookiecutter.license }}"
 ORGANIZATION = "{{ cookiecutter.github_name }}"
 
-# We need these values to generate github repository:
+# generate github repository:
 GITHUB_USER = "{{ cookiecutter.github_name }}"
 
 licenses = {
@@ -24,7 +24,7 @@ licenses = {
 
 
 def generate_license() -> None:
-    """Generates license file for the project."""
+    """generates license file for the project"""
     license_result = os.system(
         f"lice {licenses[LICENSE]} -o '{ORGANIZATION}' -p {PROJECT_NAME} > {PROJECT_DIRECTORY}/LICENSE"
     )
@@ -33,17 +33,17 @@ def generate_license() -> None:
 
 
 def print_futher_instuctions() -> None:
-    """Shows user what to do next after project creation."""
+    """shows user what to do next after project creation."""
     message = f"""
-    Your project {PROJECT_NAME} is created.
+    your project {PROJECT_NAME} is created.
 
-    1) Now you can start working on it:
+    [0] now u can start working on it:
 
         $ cd {PROJECT_NAME} && git init
 
-    4) Upload initial code to GitHub (ensure you've run `make install` to use `pre-commit`):
+    [1] upload initial code to github (ensure you've run `make install` to use `pre-commit`):
 
-        $ git add .
+        $ git add .git
         $ git commit -m "initial commit"
         $ git remote add origin https://github.com/{GITHUB_USER}/{PROJECT_NAME}.git
         $ git push -u origin master
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -7,7 +7,7 @@ MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 module_name = "{{ cookiecutter.module_name }}"
 
 if not re.match(MODULE_REGEX, module_name):
-    print(f"ERROR: {module_name} is not a valid Python module name!")
+    print(f"error: {module_name} is not a valid module name!")
 
     # exits with status 1 to indicate failure
     sys.exit(1)
