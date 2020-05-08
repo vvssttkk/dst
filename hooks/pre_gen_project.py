60dbde431b22a399632b8af6552d88f0c04eb9f9
viktor trokhymenko <trokhymenkoviktor@gmail.com>
2020-05-08 22:16:58 +0300

    upadate pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,10 +4,10 @@ import sys
 
 MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
-module_name = "{{ cookiecutter.module_name }}"
+project_name = "{{ cookiecutter.project_name }}"
 
-if not re.match(MODULE_REGEX, module_name):
-    print(f"error: {module_name} is not a valid module name!")
+if not re.match(MODULE_REGEX, project_name):
+    print(f"error: {project_name} is not a valid module name!")
 
     # exits with status 1 to indicate failure
     sys.exit(1)
